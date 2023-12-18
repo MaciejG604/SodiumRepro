9b62c828294977cd5c422a942323fb2daa27cca8
Maciej Gajek <mgajek@virtuslab.com>
2023-12-18 16:30:51 +0100

    Update workflow

--- Main.scala
+++ Main.scala
@@ -1,9 +1,12 @@
-//> using dep org.virtuslab.scala-cli:libsodiumjni:0.0.3
+//> using dep org.virtuslab.scala-cli:libsodiumjni:0.0.4
+// //> using dep com.lihaoyi::os-lib:0.9.2
 
 import libsodiumjni.Sodium
 import libsodiumjni.internal.LoadLibrary
 
 object Main extends App {
     LoadLibrary.initializeFromResources()
+    
     Sodium.init()
+    println("Hello Sodium§")
 }
